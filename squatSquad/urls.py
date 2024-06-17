ab1f44296dc3932d873b2a3ead555cb7fe9964b4
Y-Tatsuma <123883467+Y-Tatsuma@users.noreply.github.com>
2024-06-17 23:56:56 +0900

    応援ボタンが押されたときに実行されるAPI実装

--- squatSquad/urls.py
+++ squatSquad/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path("", index, name="index"),
     path("total", total, name="total"),
     path("isExercising", isExercising, name="isExercising"),
+    path("cheering_red", cheering_red, name="cheering_red"),
+    path("cheering_white", cheering_white, name="cheering_white"),
 ]

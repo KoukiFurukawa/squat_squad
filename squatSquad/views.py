0a9611382c135ec6aabc8308c6173041d749fcfb
Furukawa Kouki <94515801+kouki928@users.noreply.github.com>
2024-06-17 09:22:40 +0900

    Djangoの初期設定としてurlsを追加

--- squatSquad/views.py
+++ squatSquad/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http.response import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+def index(request):
+    return HttpResponse("テスト用")
+
+# sample --------------------------------------------------------------
+@csrf_exempt
+def reservation(request):
+    if request.method == "GET":
+        return render(request, "frontend/index.html")

077cbfdd3ebde3b1bdd9425a498401b3d5934de0
Furukawa Kouki <94515801+kouki928@users.noreply.github.com>
2024-06-24 23:50:32 +0900

    socket通信の実装 (#32)
    * socket
    
    * count-socket

--- squat_squad/asgi.py
+++ squat_squad/asgi.py
@@ -11,6 +11,19 @@ import os
 
 from django.core.asgi import get_asgi_application
 
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from squatSquad.socket_routing import websocket_urlpatterns
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "squat_squad.settings")
 
 application = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns,
+        )
+    ),
+})

2181f114a6ed7e07ec4a178a3fdbb24a83d179bf
Y-Tatsuma <ok220212@oks.iput.ac.jp>
2024-06-24 20:34:38 +0900

    chottokaeta

--- squatSquad/src/components/Home/Home.tsx
+++ squatSquad/src/components/Home/Home.tsx
@@ -13,6 +13,7 @@ const Home = () =>
     const [exercise_pre, setExercise_pre] = useState("");
     const [alert_name,setAlert_name] = useState("");
     const [alert_exer,setAlert_exer] = useState("");
+    const [viewQR, setViewQR] = useState<boolean>(false)
 
     const toggleLoginState = () => {
         if (isLogin) { 
@@ -24,6 +25,18 @@ const Home = () =>
         }
         else {
             setLogin(true)
+        }
+    };
+
+    const changeExer = (btnID: string) => {
+        setExercise_pre(btnID);
+
+        const buttons = document.querySelectorAll(".btnbox button");
+        buttons.forEach(button => {button.classList.remove("selected");});
+
+        const clickedButton = document.getElementById(btnID);
+        if (clickedButton) {
+            clickedButton.classList.add("selected");
         }
     };
 
@@ -69,26 +82,41 @@ const Home = () =>
                         </div>
                         <div>
                             <div>{`運動の得意不得意を入力して下さい${alert_exer}`}</div>
-                            <input type="text" value={exercise_pre} onChange={(e)=>{setExercise_pre(e.target.value)}} />
+                            <div className="btnbox">
+                                <button id="1" onClick={(e) => changeExer(e.currentTarget.id)}>1</button>
+                                <button id="2" onClick={(e) => changeExer(e.currentTarget.id)}>2</button>
+                                <button id="3" onClick={(e) => changeExer(e.currentTarget.id)}>3</button>
+                                <button id="4" onClick={(e) => changeExer(e.currentTarget.id)}>4</button>
+                                <button id="5" onClick={(e) => changeExer(e.currentTarget.id)}>5</button>
+                            </div>
                         </div>
                         <button type="submit" onClick={()=>{check_input()}}>OK</button>
                     </div>
                 </div>
             :<>
-            <div className="home">
-                <div className="mydata">
-                    <div>{`${name}`}</div>
-                    <div>{`スクワット回数 : ${exercise_pre}`}</div>
-                    <div>最大応援タップ数 : 100</div>
-                </div>
-                <div className="teamdata">
-                    <div>赤チーム</div>
-                    <div>チーム総得点 : </div>
-                </div>
-                <div className="buttons">
-                    <button onClick={()=>{}}>QRコードの表示</button>
-                    <button onClick={()=>{}}>応援する</button>
-                </div>
+                <div className="home">
+                    <div className="mydata">
+                        <div>{`${name}`}</div>
+                        <div>{`スクワット回数 : ${exercise_pre}`}</div>
+                        <div>最大応援タップ数 : 100</div>
+                    </div>
+                    <div className="teamdata">
+                        <div>赤チーム</div>
+                        <div>チーム総得点 : </div>
+                    </div>
+                    { viewQR ?
+                        <div className="QRmodal">
+                            <img src="" alt="QR" />
+                            <button onClick={() => setViewQR(false)}>閉じる</button>
+                        </div>
+                    :<>
+                        <div className="buttons">
+                        <button onClick={()=>{setViewQR(true)}}>QRコードの表示</button>
+                        <button onClick={()=>{}}>応援する</button>
+                        </div>
+                    </>
+                    }
+                
                 
             </div>
             <button onClick={()=>{reset_data()}}>reset</button>
